2302852f6e0fcb32520226bc1ab8c72d931a33f4
Omar Zagonel El Laden <omarzladen@gmail.com>
2025-07-21 11:15:23 -0300

    rm sh script not intended

--- setup_desktop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "[Desktop Entry]
-Name=GTK Hafiz
-Comment=Track Qur'an memorization visually
-Exec=sh $HOME/GTK-Hafiz/launcher.sh
-Type=Application
-Icon=$HOME/GTK-Hafiz/imgs/icon.png" > "$HOME"/.local/share/applications/gtk-hafiz.desktop
