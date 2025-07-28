9f34662d599e067752797699c71110200d96c3ff
Omar <omarzagonel@gmail.com>
2025-07-27 22:24:42 -0300

    better identation

--- setup_db.sh
+++ setup_db.sh
@@ -10,7 +10,8 @@ fi
 sqlite3 "$DB_FILENAME" <<EOF
 
 CREATE TABLE IF NOT EXISTS
-books(
+books
+(
     id          INTEGER PRIMARY KEY,
     name_arabic TEXT DEFAULT '',
     name_latin  TEXT DEFAULT '',
@@ -21,7 +22,8 @@ books(
 );
 
 CREATE TABLE IF NOT EXISTS
-chapters(
+chapters
+(
     number      INTEGER PRIMARY KEY,
     name_arabic TEXT DEFAULT '',
     name_latin  TEXT DEFAULT '',
@@ -31,7 +33,8 @@ chapters(
 );
 
 CREATE TABLE IF NOT EXISTS
-users(
+users
+(
     username       TEXT PRIMARY KEY,
     n_mem_chapters INTEGER DEFAULT 0,
     n_mem_words    INTEGER DEFAULT 0,
@@ -40,7 +43,8 @@ users(
 );
 
 CREATE TABLE IF NOT EXISTS
-mem_chapters(
+mem_chapters
+(
     users_username  INTEGER REFERENCES users(username),
     chapters_number INTEGER REFERENCES chapters(number)
 );
