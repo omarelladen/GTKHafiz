46c2911ba53e8c1cfe96e113fd3d19d47e604fd5
Omar Zagonel El Laden <omarzladen@gmail.com>
2026-01-01 14:35:05 -0300

    adapt install script for root

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,21 @@
 # Include config variables
 . "$PWD"/config
 
+
+SUDO=
+SUDO_U=
+
+if command -v sudo >/dev/null 2>&1 \
+   && [ "$(id -u 2>/dev/null)" = "0" ] \
+   && [ -n "$SUDO_USER" ]; then
+    SUDO=sudo
+    SUDO_U="sudo -u $SUDO_USER"
+fi
+
+echo "SUDO=$SUDO"
+echo "SUDO_U=$SUDO_U"
+echo "SUDO_USER=$SUDO_USER"
+
 scripts/uninstall.sh
 
 expand_home()
@@ -23,14 +38,14 @@ PREFERENCES_DIR=$(expand_home "$PREFERENCES_DIR")
 PREFERENCES_FILE=$(expand_home "$PREFERENCES_FILE")
 
 mkdir -pv "$BIN_DIR" "$DATA_DIR" "$ICONS_DIR" "$PYTHON_PKG_DIR" "$DESKTOP_DIR"
-sudo -u "$SUDO_USER" mkdir -pv "$DB_DIR" "$PREFERENCES_DIR"
+eval "$SUDO_U mkdir -pv \"$DB_DIR\" \"$PREFERENCES_DIR\""
 
 cp -v "$ORIG_SRC_DIR"/* "$PYTHON_PKG_DIR"
 cp -v "$ORIG_ICONS_DIR"/* "$ICONS_DIR"
 cp -v config "$DATA_DIR"
-sudo -u "$SUDO_USER" cp -v "$ORIG_DB_FILE" "$DB_FILE"
+eval "$SUDO_U cp -v \"$ORIG_DB_FILE\" \"$DB_FILE\""
 cp -v "$ORIG_BAR_SIZES_FILE" "$DATA_DIR"
-sudo -u "$SUDO_USER" cp -v "$ORIG_PREFERENCES_FILE" "$PREFERENCES_FILE"
+eval "$SUDO_U cp -v \"$ORIG_PREFERENCES_FILE\" \"$PREFERENCES_FILE\""
 
 echo "# This directory is a Python package." > "$PYTHON_PKG_DIR"/__init__.py
 
