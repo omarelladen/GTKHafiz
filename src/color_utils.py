d8fbbaf504cbea02687e7ad435ea25ce229a5c5f
Omar Zagonel El Laden <omarzladen@gmail.com>
2026-01-28 18:04:43 -0300

    rm unneeded safe var for immutable obj

--- src/color_utils.py
+++ src/color_utils.py
@@ -8,8 +8,8 @@ class ColorUtils():
         pass
 
     def is_valid_color_hex(self, color_hex):
-        _color_hex = color_hex.strip().lstrip("#").lower()
-        if re.fullmatch(r"[0-9a-f]{6}|[0-9a-f]{8}", _color_hex):
+        color_hex = color_hex.strip().lstrip("#").lower()
+        if re.fullmatch(r"[0-9a-f]{6}|[0-9a-f]{8}", color_hex):
             return True
         return False
 
@@ -21,11 +21,11 @@ class ColorUtils():
         return f"#{r:02X}{g:02X}{b:02X}{a:02X}"
 
     def hex_to_rgba(self, color_hex):
-        _color_hex = color_hex.lstrip('#')
+        color_hex = color_hex.lstrip('#')
 
-        r = int(_color_hex[0:2], 16) / 255.0
-        g = int(_color_hex[2:4], 16) / 255.0
-        b = int(_color_hex[4:6], 16) / 255.0
-        a = int(_color_hex[6:8], 16) / 255.0 if len(color_hex) == 8 else 1.0
+        r = int(color_hex[0:2], 16) / 255.0
+        g = int(color_hex[2:4], 16) / 255.0
+        b = int(color_hex[4:6], 16) / 255.0
+        a = int(color_hex[6:8], 16) / 255.0 if len(color_hex) == 8 else 1.0
 
         return Gdk.RGBA(r,g,b,a)
