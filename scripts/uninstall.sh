2de4f1b6882c6028e37f75ee524b7ba058f5a174
Omar Zagonel El Laden <omarzladen@gmail.com>
2026-02-08 12:41:23 -0300

    rm tabs

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -6,14 +6,14 @@
 
 expand_home()
 {
-	_PATH="$1"
+    _PATH="$1"
 
-	case "$_PATH" in
-		"~"|"~"/*)
-			_PATH="/home/${SUDO_USER:-$USER}${_PATH#\~}" ;;
-	esac
+    case "$_PATH" in
+        "~"|"~"/*)
+            _PATH="/home/${SUDO_USER:-$USER}${_PATH#\~}" ;;
+    esac
 
-	echo "$_PATH"
+    echo "$_PATH"
 }
 
 DB_DIR=$(expand_home "$DB_DIR")
