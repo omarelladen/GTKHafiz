9afcbdd72a0d8b96bde994236a8e50442f1cb8c1
Omar Zagonel El Laden <omarzladen@gmail.com>
2026-03-08 11:45:51 -0300

    leave home files untouched when using uninstall.sh

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
-# Include config variables
-. "$PWD"/configs
-
-. "$PWD"/scripts/expand_home.sh
-
-
 RM_FILES=files.txt
-DB_FILE=$(expand_home "$DB_FILE")
-PREFERENCES_FILE=$(expand_home "$PREFERENCES_FILE")
 
 sed -i 's|^usr/|/usr/|' "$RM_FILES"
 xargs rm -vrf < "$RM_FILES"
-rm -vf "$DB_FILE" "$PREFERENCES_FILE"
