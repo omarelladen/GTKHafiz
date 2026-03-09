c80259028e91d4d55f4ef230707cddeeec660559
Omar Zagonel El Laden <omarzladen@gmail.com>
2026-03-08 21:50:07 -0300

    add more pkg info to setup.py

--- setup.py
+++ setup.py
@@ -9,25 +9,55 @@ with open(config_file, "r") as f:
 
 
 app_name = configs.get("APP_NAME_LOWER")
+keywords = configs.get("KEYWORDS").split(";")
+pkg_dir  = "src"
+license  = "GPLv3"
+
+
+long_description_content_type = "text/markdown"
+with open("README.md", "r") as f:
+    lines = f.readlines()
+filtered_lines = [ln for ln in lines if not ln.startswith("<")]  # rm tags
+long_description = "".join(filtered_lines)
+
 
 data_files = [
-    ("share/man/man1", [configs.get("ORIG_MAN_FILE")]),
-    ("share/applications", [configs.get("ORIG_DESKTOP_FILE")]),
+    ("share/man/man1",                 [configs.get("ORIG_MAN_FILE")]),
+    ("share/applications",             [configs.get("ORIG_DESKTOP_FILE")]),
     ("share/icons/hicolor/64x64/apps", [configs.get("ORIG_ICON_FILE")]),
     (f"share/{app_name}", [
         config_file,
-        configs.get("ORIG_DB_SCRIPT"),
         configs.get("ORIG_BAR_SIZES_FILE"),
         configs.get("ORIG_BOOKS_FILE"),
         configs.get("ORIG_CHAPTERS_FILE"),
+        configs.get("ORIG_DB_SCRIPT"),
     ]),
 ]
 
+classifiers = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Operating System :: POSIX :: Linux",
+    "Environment :: X11 Applications :: GTK",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Topic :: Education",
+    "Topic :: Religion",
+]
+
+
 setup(
-    name=configs.get("APP_NAME_LOWER"),
-    version=configs.get("APP_VERSION"),
-    package_dir={app_name: "src"},
-    packages=[app_name],
-    scripts=[configs.get("ORIG_BIN_FILE")],
-    data_files=data_files,
+    name                          = app_name,
+    version                       = configs.get("APP_VERSION"),
+    author                        = configs.get("AUTHORS"),
+    url                           = configs.get("WEBSITE_URL"),
+    description                   = configs.get("APP_DESCRIPTION"),
+    long_description              = long_description,
+    long_description_content_type = long_description_content_type,
+    classifiers                   = classifiers,
+    keywords                      = keywords,
+    license                       = license,
+    package_dir                   = {app_name: pkg_dir},
+    packages                      = [app_name],
+    data_files                    = data_files,
+    scripts                       = [configs.get("ORIG_BIN_FILE")],
 )
