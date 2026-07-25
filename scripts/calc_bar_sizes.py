119888cc5a2ea311d4739c4241369dde4d0f0bb1
Omar Zagonel El Laden <omarzladen@gmail.com>
2026-07-25 09:28:56 -0300

    clean script

--- scripts/calc_bar_sizes.py
+++ scripts/calc_bar_sizes.py
@@ -4,18 +4,20 @@
 import csv
 
 
+exec(open("configs").read())
+
+
 pb_line_width = 500  # total line length of juz'
 
-exec(open('configs').read())
 
 n_lines = []
-with open(LINES_FILE, mode='r') as file:
+with open(LINES_FILE, mode="r") as file:
     reader = csv.reader(file)
     for line in reader:
         n_lines.append(line)
 
 lines = []
-with open(ORIG_BAR_SIZES_FILE, mode='r') as file:
+with open(ORIG_BAR_SIZES_FILE, mode="r") as file:
     reader = csv.reader(file)
     for line in reader:
         value = float(line[2]) * pb_line_width / float(n_lines[int(line[0])-1][1])
@@ -23,10 +25,9 @@ with open(ORIG_BAR_SIZES_FILE, mode='r') as file:
             line[3] = value
         else:
             line.append(value)
-
         lines.append(line)
 
-with open(ORIG_BAR_SIZES_FILE, mode='w', newline='') as file:
+with open(ORIG_BAR_SIZES_FILE, mode="w", newline="") as file:
     writer = csv.writer(file)
     for line in lines:
         writer.writerow(line)
